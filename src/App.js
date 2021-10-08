dfe6c03ad4d60ab385c72bdbf25a85f569652db6
Sazzad Hossain <sazzad.csebubt@gmail.com>
2021-10-08 18:25:52 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
 import './App.css';
+import { getAuth, signInWithPopup, GoogleAuthProvider, GithubAuthProvider, signOut } from "firebase/auth";
 
+import initializeAuthentication from './Firebase/firebase.init';
+import { useState } from 'react';
+const googleProvider = new GoogleAuthProvider();
+const githubProvider = new GithubAuthProvider();
+
+
+initializeAuthentication();
 function App() {
+  const [user, setUser] = useState({})
+
+  const [gituser, setGituser] = useState({});
+
+  const googleHandleButton = () => {
+    const auth = getAuth();
+    signInWithPopup(auth, googleProvider)
+      .then(result => {
+        const { displayName, email, photoURL } = result.user;
+        const logginUser = {
+          name: displayName,
+          email: email,
+          photo: photoURL
+        }
+        // console.log(user);
+        setUser(logginUser);
+      })
+      .catch(error => {
+        console.log(error.message);
+      })
+  }
+  const githubHandleButton = () => {
+    const auth = getAuth();
+    signInWithPopup(auth, githubProvider)
+      .then(result => {
+        const { displayName, email, photoURL } = result.user;
+        // console.log(user);
+        const logginGithub = {
+          name: displayName,
+          photo: photoURL,
+          email: email
+        }
+        setGituser(logginGithub)
+      })
+
+  }
+  const signoutbtn = () => {
+    const auth = getAuth();
+    signOut(auth).then(() => {
+      setUser({});
+      setGituser({});
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!((user.email) || (gituser.name)) ?
+        <div>
+          <button onClick={googleHandleButton}>Google SignIn</button>
+          <button onClick={githubHandleButton}>GitHub SignIn</button>
+        </div> :
+        <button onClick={signoutbtn}>Sign Out</button>
+      }
+      {
+        user.email && <div>
+          <h3>Welocme {user.name}</h3>
+          <p>Email:{user.email}</p>
+          <img src={user.photo} alt="" />
+        </div>
+      }
+      {
+        gituser.name && <div>
+          <h3>name:{gituser.name}</h3>
+          <p>Email:{gituser.email}</p>
+          <img src={gituser.photo} alt="" />
+        </div>
+      }
     </div>
   );
 }
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBFd37_nPhsQrgfo5H9lzTZ8zFP1m75dww",
+    authDomain: "simple-firebase-ath.firebaseapp.com",
+    projectId: "simple-firebase-ath",
+    storageBucket: "simple-firebase-ath.appspot.com",
+    messagingSenderId: "827446023795",
+    appId: "1:827446023795:web:89faae330c77ebc1c57c06"
+};
+export default firebaseConfig;
